f3c50a62277c69c24d9622dd4001df9e0c9a7245
Kiran Raj <kiranrajb5882@gmail.com>
2023-11-21 22:07:45 +0530

    add files

--- ArrayList/Convert_an_Array_Into_a_2D_Array_With_Conditions.java
+++ ArrayList/Convert_an_Array_Into_a_2D_Array_With_Conditions.java
@@ -6,21 +6,21 @@ public class Convert_an_Array_Into_a_2D_Array_With_Conditions {
     }
     public  static List<List<Integer>> findMatrix(int[] nums) {
         List<List<Integer>>result=new ArrayList<>();
-        List<Integer> num= Arrays.(nums);
-        int s=0;
-        while (true){
-            List<int[]> a=new ArrayList<>();
-            for (int i = 0; i <num.size() ; i++) {
-                if (!a.contains(num.get(i))){
-                    a.add(num.get(i));
-                    s++;
-                    num.remove(num.get(i));
-                }
-            }
-             result.add(a.);
-            if (s==nums.length)break;
-
-        }
+//        List<Integer> num= Arrays.(nums);
+//        int s=0;
+//        while (true){
+//            List<int[]> a=new ArrayList<>();
+//            for (int i = 0; i <num.size() ; i++) {
+//                if (!a.contains(num.get(i))){
+////                    a.add(num.get(i));
+//                    s++;
+//                    num.remove(num.get(i));
+//                }
+//            }
+//             result.add(a.);
+//            if (s==nums.length)break;
+//
+//        }
 
     return result;
     }
