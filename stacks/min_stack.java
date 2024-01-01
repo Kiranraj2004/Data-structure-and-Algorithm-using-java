e19dcab19f7af7a9febf0edf89dc33de40f5bc7d
Kiran Raj <kiranrajb5882@gmail.com>
2024-01-01 22:47:53 +0530

    stackquestion

---
+++ stacks/min_stack.java
@@ -0,0 +1,62 @@
+package stacks;
+
+import java.util.Stack;
+
+public class min_stack {
+    public static void main(String[] args) {
+
+        min_stack w=new min_stack();
+//        w.push(1);
+//        System.out.println(w.getMin());
+//        w.push(0);
+//        System.out.println(w.getMin());
+//        w.pop();
+//        System.out.println(w.getMin());
+        w.push(46);
+        w.push(46);
+        w.push(47);
+        w.pop();
+        w.pop();
+        w.pop();
+        w.push(47);
+        System.out.println(w.getMin());
+
+
+    }
+
+   private Stack<Integer>s;
+    int []min;
+    int i=0;
+    public min_stack() {
+        s=new Stack<>();
+        min=new int[10];
+    }
+    public void push(int val) {
+        if (s.isEmpty()){
+            s.push(val);
+            i=0;
+            min[i]=0;
+            return;
+        }
+        s.push(val);
+        if (s.get(min[i])>val){
+            min[++i]=s.size()-1;
+        }
+
+    }
+
+    public void pop() {
+      s.pop();
+      if (min[i]>s.size()-1){
+          i--;
+      }
+    }
+
+    public int top() {
+    return s.peek();
+    }
+
+    public int getMin() {
+   return s.get(min[i]);
+    }
+}
