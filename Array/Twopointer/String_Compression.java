c314c536158305e1555859aff4398c36fc200adb
Kiran Raj <kiranrajb5882@gmail.com>
2024-01-28 22:53:11 +0530

    add

--- Array/Twopointer/String_Compression.java
+++ Array/Twopointer/String_Compression.java
@@ -1,20 +1,37 @@
 package Array.Twopointer;
 
+import java.util.Arrays;
+
 public class String_Compression {
     public static void main(String[] args) {
-        char[]nums={'a','b','b','b','b','b','b','b','b','b','b','b','b',};
+//        "a","a","b","b","c","c","c"
+        char[]nums={'b','b','b','b','b','b','b','b',
+                'b','b'};
         System.out.println(compress(nums));
+        System.out.println(Arrays.toString(nums));
+       char []s= Integer.toString(nums.length).toCharArray();
+        System.out.println(Arrays.toString(s));
     }
     public  static int compress(char[] nums) {
         int i=0;
-        int j=0;
-        int count=0;
-        while(i<nums.length){
-            while(j< nums.length&&nums[j+1]==nums[i]){
-                j++;
+        int n=0;
+        while(i<nums.length) {
+            int j = 1;
+            while (i+j < nums.length) {
+                if (nums[i+j] == nums[i]) {
+                    j++;
+                } else {
+                    break;
+                }
             }
-            if (j>0&&j)
+            nums[n++]=nums[i];
+            if(j>1){
+                for(char d:Integer.toString(j).toCharArray()){
+                    nums[n]=d;
+                    n++;
+                }}
+            i += j;
         }
-
+        return n;
     }
 }
