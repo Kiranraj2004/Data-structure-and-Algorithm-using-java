8bf3e88a638ea109e8e41cd82922e043bbfeb203
Kiran Raj <kiranrajb5882@gmail.com>
2024-02-04 23:12:40 +0530

    add this

--- binary_search/Arranging_Coins.java
+++ binary_search/Arranging_Coins.java
@@ -4,14 +4,32 @@ public class Arranging_Coins {
         int n=6;
         System.out.println(arrangecoin(n));
     }
-    public static int arrangecoin(int num){
-        int i=0;
-        while(num>=0){
-            num-=i+1;
-            if (num>=0){
-                i++;
+    public static int arrangecoin(int n){
+//        int i=0;
+//        while(num>=0){
+//            num-=i+1;
+//            if (num>=0){
+//                i++;
+//            }
+//        }
+//        return i;
+
+        long start=1;
+        long end=n;
+        long mid=-1;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            long coins=(long)(mid*(mid+1))/2;
+            if(coins==n){
+                return (int) mid;
+            }
+            else if(coins>n){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
             }
         }
-        return i;
+        return(int) end;
     }
 }
