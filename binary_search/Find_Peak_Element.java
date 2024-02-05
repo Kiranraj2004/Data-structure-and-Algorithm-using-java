af6fde80589d6ebe8ef8db74609631525061c5a5
Kiran Raj <kiranrajb5882@gmail.com>
2024-02-05 22:56:08 +0530

    add

--- binary_search/Find_Peak_Element.java
+++ binary_search/Find_Peak_Element.java
@@ -2,8 +2,8 @@ package binary_search;
 
 public class Find_Peak_Element {
     public static void main(String[] args) {
-        int[]arr={10};
-        System.out.println(findPeakElement(arr));
+        int[]arr={1,2,1,3,5,6,4};
+        System.out.println(findPeakindex(arr));
 
     }
     public static int findPeakElement(int[] nums) {
@@ -17,5 +17,20 @@ public class Find_Peak_Element {
             else end--;
         }
      return start;
+    }
+    public static int findPeakindex(int[] nums) {
+        int start=0;
+        int end= nums.length-1;
+        int mid;
+        while(start<end){
+            mid=start+(end-start)/2;
+            if (nums[mid]>nums[mid+1]){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return start;
     }
 }
