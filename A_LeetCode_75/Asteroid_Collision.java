5458393a67341742986b5b4d58e838bc67af641d
Kiran Raj <kiranrajb5882@gmail.com>
2024-02-28 21:15:14 +0530

    add

--- A_LeetCode_75/Asteroid_Collision.java
+++ A_LeetCode_75/Asteroid_Collision.java
@@ -1,47 +1,52 @@
 package A_LeetCode_75;
-
-
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Asteroid_Collision {
     public static void main(String[] args) {
-        int[]a={10,10,-5};
-        System.out.println(asteroidCollision(a));
+        int[]a={10,2,-5};
+        System.out.println(Arrays.toString(asteroidCollision(a)));
     }
-    public static Stack<Integer> asteroidCollision(int[] asteroids) {
+    public static int[] asteroidCollision(int[] asteroids) {
         Stack<Integer>res=new Stack<>();
-        res.push(asteroids[0]);
-        for (int i = 1; i <asteroids.length ; i++) {
-            if (asteroids[i]>0&&res.peek()>0||asteroids[i]<0&&res.peek()<0){
+        for (int i = 0; i <asteroids.length ; i++) {
+            if (res.isEmpty()){
                 res.push(asteroids[i]);
             }
-            else{
-                int n=asteroids[i];
-                int peek=res.peek();
-                boolean q=false;
-                while (res.size()>1){
-                    if (Math.abs(peek)>Math.abs(n)){
+            else if ((asteroids[i]>0&&res.peek()>0)||(asteroids[i]<0&&res.peek()<0)){
+                res.push(asteroids[i]);
+            }
+            else {
+                int n = asteroids[i];
+                int peek = res.peek();
+                boolean q = false;
+                while (res.size() >=1) {
+                    if (Math.abs(peek) > Math.abs(n)&&((asteroids[i]>0&&res.peek()>0)||(asteroids[i]<0&&res.peek()<0)) ){
                         break;
-                    }
-                    else if(Math.abs(peek)==Math.abs(n)){
+                    } else if (Math.abs(peek) == Math.abs(n)) {
                         res.pop();
                         break;
-                    }
-                    else{
+                    } else {
                         res.pop();
-                        if (q){
+                        if (q) {
                             res.pop();
                         }
-                        if (res.size()>0){
-                        peek= res.peek();
+                        if (res.size() > 0) {
+                            peek = res.peek();
                         }
                         res.push(n);
-                        q=true;
+                        q = true;
                     }
                 }
-                return res;
             }
-        }
-        return res;
+            }
+         if (!res.empty()){
+             int[]r=new int[res.size()];
+             for (int i = 0; i < res.size() ; i++) {
+                 r[i]=res.get(i);
+             }
+             return r;
+         }
+        return new int[0];
     }
 }
