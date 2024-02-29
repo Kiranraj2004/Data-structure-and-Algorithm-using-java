c7ed3a1f1091e3ac7449315cb0dffa687da42d9c
Kiran Raj <kiranrajb5882@gmail.com>
2024-02-29 20:41:20 +0530

    add

--- A2Z_Strivers_DSA/stacksandqueues/postfix_prefix_infix/Evaluate_Reverse_Polish_Notation.java
+++ A2Z_Strivers_DSA/stacksandqueues/postfix_prefix_infix/Evaluate_Reverse_Polish_Notation.java
@@ -1,17 +1,44 @@
 package A2Z_Strivers_DSA.stacksandqueues.postfix_prefix_infix;
 
+import java.util.Objects;
 import java.util.Stack;
 
 public class Evaluate_Reverse_Polish_Notation {
     public static void main(String[] args) {
-        String []s={"2","1","+","3","*"};
+        String []s={"4","13","5","/","+"};
+        System.out.println(evalRPN(s));
+//        System.out.println(number("-239"));
     }
     public static  int evalRPN(String[] tokens) {
         Stack<Integer>res=new Stack<>();
-        for (int i = 0; i <tokens.length ; i++) {
-            if (tokens[i]!="+"||tokens[i]!="-"||tokens[i]!="/"||tokens[i]!="*"){
-                char num=(char)tokens[i].charAt(0);
+        for (String token : tokens) {
+            if (Objects.equals(token, "+") || Objects.equals(token, "-") ||
+                    (Objects.equals(token, "/")) || (Objects.equals(token, "*"))) {
+                int second = res.pop();
+                int first = res.pop();
+                switch (token) {
+                    case "+" -> res.push(first + second);
+                    case "-" -> res.push(first - second);
+                    case "*" -> res.push(first * second);
+                    case "/" -> res.push(first / second);
+                }
+            } else {
+                res.push(number(token));
             }
         }
+        return res.pop();
+    }
+    static int number(String s){
+        int num=0;
+        for (int i = 0; i <s.length() ; i++) {
+          if (s.charAt(i)!='-') {
+              num *= 10;
+              num += (int) s.charAt(i) - 48;
+          }
+        }
+        if (s.charAt(0)=='-'){
+            num*=-1;
+        }
+        return num;
     }
 }
--- A_LeetCode_75/Asteroid_Collision.java
+++ A_LeetCode_75/Asteroid_Collision.java
@@ -4,49 +4,44 @@ import java.util.Stack;
 
 public class Asteroid_Collision {
     public static void main(String[] args) {
-        int[]a={10,2,-5};
+        int[]a={10,2,-15};
         System.out.println(Arrays.toString(asteroidCollision(a)));
     }
     public static int[] asteroidCollision(int[] asteroids) {
         Stack<Integer>res=new Stack<>();
         for (int i = 0; i <asteroids.length ; i++) {
-            if (res.isEmpty()){
-                res.push(asteroids[i]);
-            }
-            else if ((asteroids[i]>0&&res.peek()>0)||(asteroids[i]<0&&res.peek()<0)){
-                res.push(asteroids[i]);
-            }
-            else {
-                int n = asteroids[i];
-                int peek = res.peek();
-                boolean q = false;
-                while (res.size() >=1) {
-                    if (Math.abs(peek) > Math.abs(n)&&((asteroids[i]>0&&res.peek()>0)||(asteroids[i]<0&&res.peek()<0)) ){
-                        break;
-                    } else if (Math.abs(peek) == Math.abs(n)) {
-                        res.pop();
-                        break;
-                    } else {
-                        res.pop();
-                        if (q) {
-                            res.pop();
-                        }
-                        if (res.size() > 0) {
-                            peek = res.peek();
-                        }
-                        res.push(n);
-                        q = true;
-                    }
-                }
-            }
-            }
-         if (!res.empty()){
-             int[]r=new int[res.size()];
-             for (int i = 0; i < res.size() ; i++) {
-                 r[i]=res.get(i);
-             }
-             return r;
+           if (res.isEmpty() || asteroids[i]>0){
+               res.push(asteroids[i]);
+           }
+           else{
+               while (true){
+                   int top= res.peek();
+                   if (top<0){
+                       res.push(asteroids[i]);
+                       break;
+                   }
+                   int abs=Math.abs(asteroids[i]);
+                   if(top==abs){
+                       res.pop();
+                       break;
+                   }
+                   else if(top>abs){
+                       break;
+                   }
+                   else{
+                       res.pop();
+                       if (res.isEmpty()){
+                           res.push(asteroids[i]);
+                           break;
+                       }
+                   }
+               }
+           }
          }
-        return new int[0];
+        int []k=new int[res.size()];
+        for (int i = 0; i < res.size() ; i++) {
+            k[i]=res.get(i);
+        }
+        return k;
     }
 }
