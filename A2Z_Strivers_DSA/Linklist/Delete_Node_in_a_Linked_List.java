69e24fc297eaa0d4e518ae4d9e4e2bae38533c46
Kiran Raj <kiranrajb5882@gmail.com>
2024-03-04 21:12:59 +0530

    add

---
+++ A2Z_Strivers_DSA/Linklist/Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,14 @@
+package A2Z_Strivers_DSA.Linklist;
+
+public class Delete_Node_in_a_Linked_List {
+    public static void main(String[] args) {
+
+    }
+
+//    https://leetcode.com/problems/delete-node-in-a-linked-list/
+//    i solved this problem
+//    public void deleteNode(ListNode node) {
+//       node.val=node.next.val;
+//       node.next=node.next.next;
+//    }
+}
