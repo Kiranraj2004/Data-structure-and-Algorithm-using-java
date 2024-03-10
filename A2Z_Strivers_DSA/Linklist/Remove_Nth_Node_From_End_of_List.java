b1d11c49a186d670679904cca278abf4963d864f
Kiran Raj <kiranrajb5882@gmail.com>
2024-03-10 23:14:33 +0530

    add

---
+++ A2Z_Strivers_DSA/Linklist/Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,44 @@
+package A2Z_Strivers_DSA.Linklist;
+
+public class Remove_Nth_Node_From_End_of_List {
+    public static void main(String[] args) {
+
+    }
+
+//    public ListNode removeNthFromEnd(ListNode head, int n) {
+//        if(head.next==null){
+//            return null;
+//        }
+//        ListNode temp=head;
+//        int count=0;
+//        while(temp!=null){
+//            count++;
+//            temp=temp.next;
+//        }
+//        int index=count-n;
+//        if(index==0){
+////     delete index at first;
+//           head=head.next;
+//           return head;
+//   }
+//        if(index==count-1){
+////            delete at last
+//            ListNode t=head;
+//            while(t.next.next!=null){
+//                t=t.next;
+//            }
+//            t.next=null;
+//            return head;
+//        }
+//        temp=head;
+//        int k=1;
+//        while(k<=index){
+//            temp=temp.next;
+//            k++;
+//        }
+//        temp.next=temp.next.next;
+//        return head;
+//    }
+
+}
+
