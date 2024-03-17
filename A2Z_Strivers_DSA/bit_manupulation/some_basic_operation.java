617ad14dff5ff7f06b45a6670eb0340aa22d554c
Kiran Raj <kiranrajb5882@gmail.com>
2024-03-17 23:07:55 +0530

    add basic

---
+++ A2Z_Strivers_DSA/bit_manupulation/some_basic_operation.java
@@ -0,0 +1,23 @@
+package A2Z_Strivers_DSA.bit_manupulation;
+
+public class some_basic_operation {
+    public static void main(String[] args) {
+        int a=13;
+        int b=5;
+//        AND operator (&)
+        System.out.println(a&b);
+//        OR operator (|)
+        System.out.println(a|b);
+//        XOR operator (^)
+        System.out.println(a^b);
+//        NOT operator (~)
+        System.out.println(~a);
+        System.out.println(~-12);
+//   largest number that int can store 2147483647 2power31-1
+//   System.out.println();
+        int s=Integer.MAX_VALUE;
+        System.out.println(s);
+//        smallest number that integer contains -2147483648 -2power31
+        System.out.println(Integer.MIN_VALUE);
+    }
+}
