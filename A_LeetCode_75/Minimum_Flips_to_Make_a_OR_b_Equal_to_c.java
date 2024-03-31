6aca4ada51634e25b073276408c9483659d569fc
Kiran Raj <kiranrajb5882@gmail.com>
2024-03-31 20:16:55 +0530

    add

--- A_LeetCode_75/Minimum_Flips_to_Make_a_OR_b_Equal_to_c.java
+++ A_LeetCode_75/Minimum_Flips_to_Make_a_OR_b_Equal_to_c.java
@@ -2,25 +2,24 @@ package A_LeetCode_75;
 
 public class Minimum_Flips_to_Make_a_OR_b_Equal_to_c {
     public static void main(String[] args) {
-        System.out.println(minFlips(7,7,7));
+        System.out.println(minFlips(6,2,5));
     }
     public static int minFlips(int a, int b, int c) {
-        if((a|b)==c)return 0;
         int count=0;
-        int andAB=a&b;
-        int andc=c&andAB;
-        if (andc==0) {
-            while (andAB != 0) {
-                count += 2;
-                andAB = andAB & (andAB - 1);
+        int a1,b1,c1;
+        while(a!=0||b!=0||c!=0){
+            a1=a&1;
+            b1=b&1;
+            c1=c&1;
+            if (c1==0){
+                count+=b1+a1;
             }
-        }
-        int XorAB=a^b;
-        int xorc=c^XorAB;
-
-        while(xorc!=0){
-            count++;
-            xorc=xorc&(xorc-1);
+            else{
+                if (a1+b1==0)count++;
+            }
+            a=a>>1;
+            b=b>>1;
+            c=c>>1;
         }
         return count;
 
