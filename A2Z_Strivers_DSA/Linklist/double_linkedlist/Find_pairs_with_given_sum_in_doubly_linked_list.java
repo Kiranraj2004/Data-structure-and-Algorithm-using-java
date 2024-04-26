8c656d9454286751cd97a935301037b25562cdc2
Kiran Raj <kiranrajb5882@gmail.com>
2024-04-26 13:35:05 +0530

    add

--- A2Z_Strivers_DSA/Linklist/double_linkedlist/Find_pairs_with_given_sum_in_doubly_linked_list.java
+++ A2Z_Strivers_DSA/Linklist/double_linkedlist/Find_pairs_with_given_sum_in_doubly_linked_list.java
@@ -4,26 +4,22 @@ public class Find_pairs_with_given_sum_in_doubly_linked_list {
     public static void main(String[] args) {
 
     }
-//    public static boolean findPair(Node<Integer> head, int k) {
-//        // Write your code here.
-//        Node<Integer>end=head;
-//        Node<Integer>start=head;
-//        while(end.next!=null){
-//            end=end.next;
+//  public static boolean findPair(Node head, int k) {
+//        Node tail = head;
+//        while (tail.next != null) {
+//            tail = tail.next;
 //        }
-//        while(start!=end){
-//            int sum=start.data+end.data;
-//            if(sum==k){
+//
+//        while (head.data < tail.data) {
+//            int sum = head.data + tail.data;
+//            if (sum == k) {
 //                return true;
-//            }else if(sum>k){
-//                end=end.prev;
-//            }
-//            else{
-//                if(start==end)break;
-//                start=start.next;
+//            } else if (sum > k) {
+//                tail = tail.prev;
+//            } else {
+//                head = head.next;
 //            }
 //        }
 //        return false;
-//
 //    }
 }
