94441f431c4465f8efb687075e002fc6c539dea1
Kiran Raj <kiranrajb5882@gmail.com>
2024-04-27 20:03:49 +0530

    add

---
+++ A2Z_Strivers_DSA/Linklist/double_linkedlist/Remove_duplicates_from_a_sorted_Doubly_Linked_List.java
@@ -0,0 +1,24 @@
+package A2Z_Strivers_DSA.Linklist.double_linkedlist;
+
+public class Remove_duplicates_from_a_sorted_Doubly_Linked_List {
+    public static void main(String[] args) {
+    }
+
+    public static Node uniqueSortedList(Node head) {
+//        Node temp=head.next;
+//        Node prev=head;
+//        while(temp!=null){
+//            if(temp.data==prev.data){
+//                prev.next=temp.next;
+//                temp=temp.next;
+//            }
+//            else{
+//                prev=temp;
+//                temp=temp.next;
+//
+//            }
+//        }
+//        return head;
+
+    }
+}
