b35784d6baceb22589759eed07099cda0ab60df4
Kiran Raj <kiranrajb5882@gmail.com>
2024-06-08 22:51:30 +0530

    add

---
+++ A2Z_Strivers_DSA/binary_search_tree/Insert_into_a_Binary_Search_Tree.java
@@ -0,0 +1,30 @@
+package A2Z_Strivers_DSA.binary_search_tree;
+
+public class Insert_into_a_Binary_Search_Tree {
+    public static void main(String[] args) {
+
+    }
+//    public TreeNode insertIntoBST(TreeNode root, int val) {
+//        TreeNode newnode=new TreeNode(val);
+//        if(root==null){
+//            return newnode;
+//        }
+//        TreeNode temp=root;
+//        while(true){
+//            if (root.val<val){
+//                if (root.right==null)break;
+//                root=root.right;
+//            }
+//            else{
+//                if (root.left==null)break;
+//                root=root.left;
+//            }
+//        }
+//        if (root.val>val){
+//            root.left=newnode;
+//        }else{
+//            root.right=newnode;
+//        }
+//        return temp;
+//    }
+}
