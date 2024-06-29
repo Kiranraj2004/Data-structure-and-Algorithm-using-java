951959b38048a0689f30001296742aa9effedacd
Kiran Raj <kiranrajb5882@gmail.com>
2024-06-29 20:16:10 +0530

    add

--- A2Z_Strivers_DSA/Graphs/shortest_path_of_graph/Implementing_Dijkstra_Algorithm.java
+++ A2Z_Strivers_DSA/Graphs/shortest_path_of_graph/Implementing_Dijkstra_Algorithm.java
@@ -4,33 +4,67 @@ public class Implementing_Dijkstra_Algorithm {
     public static void main(String[] args) {
 
     }
-    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
-    {
-        // Write your code here
-        int []distance=new int[V];
-        Arrays.fill(distance,-1);
-        Queue<Integer>q=new LinkedList<>();
-        q.add(S);
-        distance[S]=0;
-        while(!q.isEmpty()){
-            int s=q.remove();
-            int sd=distance[s];
-            for(ArrayList<Integer> i:adj.get(s)){
+//    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+//    {
+//        // Write your code here
+//        int []distance=new int[V];
+//        Arrays.fill(distance,-1);
+//        Queue<Integer>q=new LinkedList<>();
+//        q.add(S);
+//        distance[S]=0;
+//        while(!q.isEmpty()){
+//            int s=q.remove();
+//            int sd=distance[s];
+//            for(ArrayList<Integer> i:adj.get(s)){
+//
+//                    int d = i.get(0);
+//                    int w = i.get(1);
+//                    if (distance[d] == -1) {
+//                        distance[d] = sd + w;
+//                        q.add(d);
+//                    } else {
+//                        if (distance[d] > sd + w) {
+//                            distance[d] = sd + w;
+//                            q.add(d);
+//                        }
+//                    }
+//                }
+//            }
+//        return distance;
+//    }
 
-                    int d = i.get(0);
-                    int w = i.get(1);
-                    if (distance[d] == -1) {
-                        distance[d] = sd + w;
-                        q.add(d);
-                    } else {
-                        if (distance[d] > sd + w) {
-                            distance[d] = sd + w;
-                            q.add(d);
-                        }
-                    }
-                }
-            }
-        return distance;
+    class pair{
+        int distance;
+        int node;
+        pair(int d,int n){
+            this.distance=d;
+            node=n;
+        }
     }
+ int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+{
+    // Write your code here
+    int []distance=new int[V];
+    PriorityQueue<pair>q=new PriorityQueue<>((x,y) -> x.distance - y.distance);
+    Arrays.fill(distance, (int) 1e9);
+    distance[S]=0;
+    q.add(new pair(0,S));
+    while(!q.isEmpty()){
+        int d=q.peek().distance;
+        int node=q.peek().node;
+        q.poll();
+        for(ArrayList<Integer>i:adj.get(node)){
+            int edgeWeight = i.get(1);
+            int adjNode = i.get(0);
+            if(d + edgeWeight < distance[adjNode]) {
+                distance[adjNode] = d + edgeWeight;
+                q.add(new pair(distance[adjNode], adjNode));
+            }
+
+        }
+    }
+
+    return distance;
+}
 
 }
---
+++ A2Z_Strivers_DSA/Graphs/shortest_path_of_graph/Shortest_Path_in_Binary_Matrix.java
@@ -0,0 +1,56 @@
+package A2Z_Strivers_DSA.Graphs.shortest_path_of_graph;
+
+import java.util.PriorityQueue;
+
+class pair{
+    int distance;
+    int i;
+    int j;
+    public pair(int d,int i,int j) {
+        this.distance=d;
+        this.j=j;
+        this.i = i;
+    }
+}
+public class Shortest_Path_in_Binary_Matrix {
+    public static void main(String[] args) {
+        int[][]m={{0,0,0},{0,0,1},{1,1,0}};
+        System.out.println(shortestPathBinaryMatrix(m));
+
+    }
+//    time complexity O(N*log(N))
+//    space complexity O(N*N)
+    public static  int shortestPathBinaryMatrix(int[][] grid) {
+        int n=grid.length;
+        if (grid[0][0]==1||grid[n-1][n-1]==1)return -1;
+        int[][]temp=new int[n][n];
+        PriorityQueue<pair>q=new PriorityQueue<>((x,y) -> x.distance - y.distance);
+        temp[0][0]=1;
+        q.add(new pair(1,0,0));
+        int []drow={-1,1,0,0,-1,-1,1,1};
+        int []dcol={0,0,-1,1,-1,1,-1,1};
+        while(!q.isEmpty()){
+            int d=q.peek().distance;
+            int i=q.peek().i;
+            int j=q.peek().j;
+            q.poll();
+            for (int k = 0; k <drow.length ; k++) {
+                int i1=i+drow[k];
+                int j1=j+dcol[k];
+                if (i1>=0&&i1<n&&j1>=0&&j1<n&&grid[i1][j1]==0){
+                    if (i1==n-1&&j1==n-1)return d+1;
+                    if (temp[i1][j1]==0){
+                        temp[i1][j1]=d+1;
+                        q.add(new pair(d+1,i1,j1));
+                    }
+                    else if(temp[i1][j1]>d+1){
+                        temp[i1][j1]=d+1;
+                        q.add(new pair(d+1,i1,j1));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
