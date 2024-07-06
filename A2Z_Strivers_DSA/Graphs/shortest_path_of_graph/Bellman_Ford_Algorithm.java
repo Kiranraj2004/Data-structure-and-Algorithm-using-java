aed4df829048eb1f7724cfc25b2f8cd4091c0ab2
Kiran Raj <kiranrajb5882@gmail.com>
2024-07-06 21:54:49 +0530

    add

---
+++ A2Z_Strivers_DSA/Graphs/shortest_path_of_graph/Bellman_Ford_Algorithm.java
@@ -0,0 +1,34 @@
+package A2Z_Strivers_DSA.Graphs.shortest_path_of_graph;
+import java.util.*;
+public class Bellman_Ford_Algorithm {
+    public static void main(String[] args) {
+
+    }
+    static int[] bellman_ford(int V, ArrayList<ArrayList<Integer>> edges, int S) {
+
+        int []distance=new int[V];
+        Arrays.fill(distance,(int)1e8);
+        distance[S]=0;
+        int i=0;
+        while(i<V){
+            for(ArrayList<Integer>adj:edges){
+                int u=adj.get(0);
+                int v=adj.get(1);
+                int weight=adj.get(2);
+                if(distance[u]!=1e8&&distance[u]+weight<distance[v]){
+                    distance[v]=distance[u]+weight;
+                }
+            }
+            i++;
+        }
+        for(ArrayList<Integer>adj:edges){
+            int u=adj.get(0);
+            int v=adj.get(1);
+            int weight=adj.get(2);
+            if(distance[u]+weight<distance[v]){
+                return new int[]{-1};
+            }
+        }
+        return distance;
+    }
+}
