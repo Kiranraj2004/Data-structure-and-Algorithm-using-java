eef74454838bc3cb3baead0fcd345df3d422a20a
Kiran Raj <kiranrajb5882@gmail.com>
2024-11-06 22:53:04 +0530

    add

--- A2Z_Strivers_DSA/Array/Medium/Pascals_Triangle.java
+++ A2Z_Strivers_DSA/Array/Medium/Pascals_Triangle.java
@@ -7,7 +7,8 @@ public class Pascals_Triangle {
     }
 
 //     if the given question is  given row and column find the pascal number in that index
-//     optimal soulution is ncr
+//     optimal solution is ncr
+//    0 based index for row and col
      public int ncr(int n,int r){
         long res=1;
         for(int i=0;i<r;i++){
@@ -16,6 +17,12 @@ public class Pascals_Triangle {
         }
         return (int)res;
      }
+
+//     https://leetcode.com/problems/pascals-triangle/description/
+
+//     printing  the entire pascal triangle up to Nth row
+
+//     using dp method
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>>ans=new ArrayList<>();
         List<Integer>temp=new ArrayList<>();
@@ -42,4 +49,41 @@ public class Pascals_Triangle {
         }
         return ans;
     }
+//    TC O(n**2)
+//    sc O(1) we are using the space to store answer
+
+    public List<List<Integer>> generate1(int numRows) {
+        List<List<Integer>>ans=new ArrayList<>();
+        for (int i = 0; i <numRows ; i++) {
+            List<Integer>temp=new ArrayList<>();
+            for (int j = 0; j <=i ; j++) {
+                temp.add(ncr(i,j));
+            }
+            ans.add(temp);
+
+        }
+        return ans;
+    }
+    //    TC O(n*n*r)==O(n**3)
+//    sc O(1) we are using the space to store answer
+
+//    optimal solution formula method
+    public List<List<Integer>> generate2(int numRows) {
+        List<List<Integer>>ans=new ArrayList<>();
+        for (int i = 1; i <numRows ; i++) {
+            List<Integer>temp=new ArrayList<>();
+            temp.add(1);
+            long a=1;
+            for (int j = 1; j <=i ; j++) {
+                a=a*(i-j);
+                a=a/j;
+                temp.add((int)a);
+            }
+            ans.add(temp);
+        }
+        return ans;
+    }
+    //    TC O(n*n)==O(n*n)
+//    sc O(1) we are using the space to store answer
+//    https://leetcode.com/problems/pascals-triangle/description/
 }
